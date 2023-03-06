9208847c54b2b84bace666cc2edb811573d7a685
manhtx <manhtx.bhsoft@gmail.com>
2023-03-06 16:25:25 +0700

    change api key

--- src/modules/v1/chatGPT/chatGPT.module.ts
+++ src/modules/v1/chatGPT/chatGPT.module.ts
@@ -5,15 +5,21 @@ import { ChatGPTController } from './chatGPT.controller';
 import { Message, MessageSchema } from './schemas/message.schema';
 import { RoomChat, RoomChatSchema } from './schemas/roomChat.schema';
 import { RedisService } from '@root/common/utils/cache.util';
+import { HttpModule } from '@nestjs/axios';
+import { AxiosService } from '@root/common/utils/axios.util';
 
 @Module({
   imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
     MongooseModule.forFeature([
       { name: Message.name, schema: MessageSchema },
       { name: RoomChat.name, schema: RoomChatSchema },
     ]),
   ],
   controllers: [ChatGPTController],
-  providers: [ChatGPTService, RedisService],
+  providers: [ChatGPTService, RedisService, AxiosService],
 })
 export class ChatGPTModule {}
